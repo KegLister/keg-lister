c8a9d7145e442db6ac8e89783d7ccae3ed27e26f
Andrew Lange <andrew.lange13@gmail.com>
2022-04-08 16:32:54 -0500

    first push

---
+++ src/main/java/keglister/controllers/CreateAdServlet.java
@@ -0,0 +1,33 @@
+package keglister.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "CreateAdServlet", urlPatterns = "/ads/create")
+public class CreateAdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        HttpSession session = request.getSession(false); //gets current session without creating new one
+        if (session.getAttribute("user") != null) {
+            User user = (User) session.getAttribute("user");
+            Ad ad = new Ad(
+                    user.getId(),
+                    request.getParameter("title"),
+                    request.getParameter("description")
+            );
+            DaoFactory.getAdsDao().insert(ad);
+            response.sendRedirect("/ads");
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
