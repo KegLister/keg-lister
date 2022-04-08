6c2714ea9e69746c30d9ede8dac86fc0a9d52107
Andrew Lange <andrew.lange13@gmail.com>
2022-04-08 16:53:03 -0500

    copies file structure from adlister work

---
+++ src/main/java/keglister/controllers/ViewProfileServlet.java
@@ -0,0 +1,20 @@
+package keglister.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "adlister.controllers.ViewProfileServlet", urlPatterns = "/profile")
+public class ViewProfileServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+        } else {
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        }
+    }
+}
---
+++ src/main/java/keglister/dao/Users.java
@@ -0,0 +1,9 @@
+package keglister.dao;
+
+import keglister.models.User;
+
+public interface Users {
+    User findByUsername(String username);
+    Long insert(User user);
+}
+
