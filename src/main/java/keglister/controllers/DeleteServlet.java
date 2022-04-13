acc3f31b8b6956fc73059a6545d41488c83d5073
Andrew Lange <andrew.lange13@gmail.com>
2022-04-13 09:52:32 -0500

    changes response param

--- src/main/java/keglister/controllers/DeleteServlet.java
+++ src/main/java/keglister/controllers/DeleteServlet.java
@@ -13,10 +13,10 @@ import java.io.IOException;
 public class DeleteServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         long id = Long.parseLong(req.getParameter("id"));
         DaoFactory.getAdsDao().delete(id);
-        res.sendRedirect("/ads");
+        resp.sendRedirect("/ads");
 
     }
 
